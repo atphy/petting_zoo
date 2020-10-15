b9924eb1b0caf2caa31e40fbd89786da9f4149b3
Austin Phy <atphy42@gmail.com>
2020-10-15 12:36:07 -0400

    makin dem pets

---
+++ animals.py
@@ -0,0 +1,156 @@
+from datetime import date
+
+class Llama:
+
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = False
+        self.slithering = False
+        self.walking = True
+
+class Goat:
+
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = False
+        self.slithering = False
+        self.walking = True
+
+class Donkey:
+
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = False
+        self.slithering = False
+        self.walking = True
+
+class Small_Horse:
+
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = False
+        self.slithering = False
+        self.walking = True
+
+class Orangutan:
+
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = False
+        self.slithering = False
+        self.walking = True
+
+class Rattlesnake:
+
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = False
+        self.slithering = True
+        self.walking = False
+
+class Copperhead:
+
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = False
+        self.slithering = True
+        self.walking = False
+
+class Rat_Snake:
+
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = False
+        self.slithering = True
+        self.walking = False
+
+class Diamondback:
+
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = False
+        self.slithering = True
+        self.walking = False
+
+class Gorgon:
+
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = False
+        self.slithering = True
+        self.walking = False
+
+class Goldfish:
+
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = True
+        self.slithering = False
+        self.walking = False
+
+class Mallrd:
+
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = True
+        self.slithering = False
+        self.walking = False
+
+class Shark:
+
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = True
+        self.slithering = False
+        self.walking = False
+
+class Jellyfish:
+
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = True
+        self.slithering = False
+        self.walking = False
+
+class Fish_With_No_Eyes:
+
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = True
+        self.slithering = False
+        self.walking = False
+
+miss_fuzz = Llama("Miss Fuzz", "llama")
+goatse = Goat("Goatse", "goat")
+donkeh = Donkey("Donkeh", "donkey")
+little_sebastian = Small_Horse("Little Sebastian", "small horse")
